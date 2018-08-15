ccf7b69bf56c93b1277dddcef437bf845b1c467d
rockatoo <youlia73@naver.com>
2018-08-15 10:56:13 +0900

    From C To C++: Binary Literal & Digit Separator

--- to_cpp.cpp
+++ to_cpp.cpp
@@ -76,5 +76,12 @@ int main(int argc, char* argv[]) {
 		std::cout << "Different!" << std::endl;
 	*/
 	
+	int decimal = 52;
+	int octal = 064;
+	int hexadecimal = 0x34;
+	int binary = 0b110100; // C++14
+	int maxInt_Cpp98 = 2147483647;
+	// int maxInt_Cpp14 = 21'4748'3647; // C++14
+	
 	return 0;
 }
