1a8c1a0ae357870d6a06066c8878f2d6a7746784
rockatoo <youlia73@naver.com>
2018-08-15 09:44:16 +0900

    From C To C++: bool Type

--- to_cpp.cpp
+++ to_cpp.cpp
@@ -2,10 +2,21 @@
 
 int main(int argc, char* argv[]) {
 	int x = 10, y = 20;
-	std::cout << "x = " << x << ", y = " << y << std::endl;
+	std::cout << "x = " << x << ", y = " << y << std::endl << std::endl;
 
 	int input = 0;
 	std::cout << "Type Number: ";
 	std::cin >> input;
-	std::cout << "Typed Number: " << input << std::endl;
+	std::cout << "Typed Number: " << input << std::endl << std::endl;
+
+	bool b = true;
+	std::cout << "b = true: " << b << std::endl;
+	std::cout << "!b = false: " << !b << std::endl << std::endl;
+
+	std::cout.setf(std::ios::boolalpha); // marked with true/false
+	std::cout << "b = true: " << b << std::endl;
+	std::cout << "!b = false: " << !b << std::endl;
+	std::cout.unsetf(std::ios::boolalpha); // marked with 1/0
+	std::cout << "b = true: " << b << std::endl;
+	std::cout << "!b = false: " << !b << std::endl << std::endl;
 }
