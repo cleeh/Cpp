62f15bc9981c1c71993a97262ce7aba224e7152e
rockatoo <youlia73@naver.com>
2018-08-15 09:58:05 +0900

    From C To C++: range-based for

--- to_cpp.cpp
+++ to_cpp.cpp
@@ -19,4 +19,12 @@ int main(int argc, char* argv[]) {
 	std::cout.unsetf(std::ios::boolalpha); // marked with 1/0
 	std::cout << "b = true: " << b << std::endl;
 	std::cout << "!b = false: " << !b << std::endl << std::endl;
+
+	int arr[] = {1, 2, 3, 4, 5};
+	for(int i = 0; i < 5; i++)
+		std::cout << arr[i] << " ";
+	std::cout << std::endl;
+	for(auto& i : arr)
+		std::cout << i << " ";
+	std::cout << std::endl;
 }
