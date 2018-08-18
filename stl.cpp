33eb5f8f11dd698649a68611d238839148509d58
rockatoo <youlia73@naver.com>
2018-08-18 23:45:24 +0900

    Standard Template Library: Deque

--- stl.cpp
+++ stl.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <deque>
 
 int main(){
 	std::vector<int> vec;
@@ -19,7 +20,30 @@ int main(){
 	for(auto iter = vec.begin(); iter != vec.end(); iter++){
 		nSum += (*iter);
 	}
+	std::cout << "nSum: " << nSum << std::endl << std::endl;
+	
+	std::deque<int> deq;
+	deq.push_back(50);
+	deq.push_back(100);
+	deq.pop_back();
+	deq.push_front(1000);
+	deq.push_front(2000);
+	deq.pop_front();
+	deq.resize(10,0);
+	deq[3] = 10;
+	
+	std::cout << "deq[3] = 10: " << deq[3] << std::endl;
+	
+	nSum = 0;
+	for(int i = 0; i < deq.size(); i++){
+		nSum += deq[i];
+	}
 	std::cout << "nSum: " << nSum << std::endl;
+	//auto = std::iterator<std::deque<int>>
+	for(auto iter = deq.begin(); iter != deq.end(); iter++){
+		nSum += (*iter);
+	}
+	std::cout << "nSum: " << nSum << std::endl << std::endl;
 	
 	return 0;
 }
