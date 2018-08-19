c16873d6edd2dc62b0111e2aca3b7f4c6c89061f
rockatoo <youlia73@naver.com>
2018-08-19 22:17:30 +0900

    Standard Template Library: Non Change Algorithm - std::find_if

--- no-change_algorithm.cpp
+++ no-change_algorithm.cpp
@@ -3,7 +3,7 @@
 #include <algorithm>
 
 int main(){
-	std::vector<int> V{10, 20, 30, 40, 50};
+	std::vector<int> V{10, 20, 40, 30, 50};
 	
 	auto iter30 = std::find(V.rbegin(), V.rend(), 30);
 	auto iter25 = std::find(V.begin(), V.end(), 25);
@@ -12,6 +12,12 @@ int main(){
 		std::cout << "Found 30 !" << std::endl;
 	if(iter25 != V.end())
 		std::cout << "Found 25 !" << std::endl;
+	
+	auto GT25 = [](int n) {return n > 25;};
+	auto itFI = std::find_if(V.begin(), V.end(), GT25);
+	if(itFI != V.end())
+		std::cout << (*itFI) << std::endl;
+	auto ifFI = std::find_if(V.rbegin(), V.rend(), GT25);
 	
 	return 0;
 }
