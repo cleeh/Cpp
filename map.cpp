3cf5dd08da5ad03b627493c6d352f6771d59ebb0
rockatoo <youlia73@naver.com>
2018-08-19 13:41:40 +0900

    Standard Template Library: Map + Vector other than Multi Map

--- map.cpp
+++ map.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <map>
+#include <vector>
+#include <string>
 
-int main(){
+int other_main10(){
 	std::multimap<int, double> M{{8, 32.5}, {8, 1.1}};
 	M.insert(std::make_pair<int, int>(5, 105.5));
 	M.insert(std::pair<int, int>(5, 14.9));
@@ -9,6 +11,18 @@ int main(){
 	
 	for(auto iter = M.begin(); iter != M.end(); iter++){
 		std::cout << "(" << iter->first << ", " << iter->second << ")" << std::endl;
+	}
+	
+	std::string key = "sss";
+	std::map<std::string, std::vector<int>> MSV;
+	auto iter = MSV.find(key);
+	if(iter -> second.empty()){
+		
+	}
+	iter->second.push_back(10);
+	std::vector<int> &V = iter->second;
+	for(auto IT = V.begin(); IT != V.end(); IT++){
+		
 	}
 	
 	return 0;
