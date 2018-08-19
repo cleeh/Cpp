e7dd19ae83a8bf0365f94e04435671e2fa72bce4
rockatoo <youlia73@naver.com>
2018-08-19 23:14:19 +0900

    Standard Template Library: Calculating Function - std::accumulate

--- sorting-merge_algorithm.cpp
+++ sorting-merge_algorithm.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <functional>
 #include <deque>
+#include <numeric>
 
 template<typename T>
 void print_vector(std::vector<T> vec){
@@ -24,6 +25,19 @@ int main(){
 	auto isLess = [](int a, int b){ return a < b; };
 	std::merge(V.begin(), V.end(), D.begin(), D.end(), VR.begin(), isLess);
 	print_vector<int>(VR);
+	
+	std::vector<int> VA{1,2,3,4};
+	int nSum = std::accumulate(VA.begin(), VA.end(), 0); // Sum of vector
+	int nSum10 = std::accumulate(VA.begin(), VA.end(), 10); // Sum of (vector + 10)
+	std::cout << "(nSum, nSum10): (" << nSum << ", " << nSum10 << ")" << std::endl;
+	
+	auto multi = [](int a, int b) { return a * b; };
+	int nMulti = std::accumulate(VA.begin(), VA.end(), 1, multi); // Product of arguments
+	std::cout << "nMulti: " << nMulti << std::endl;
+	
+	auto nSquare = [](int a, int b) { return a + b * b; };
+	int nSumSquare = std::accumulate(VA.begin(), VA.end(), 0, nSquare); // Sum of Square
+	std::cout << "nSumSquare: " << nSumSquare << std::endl;
 	
 	return 0;
 }
