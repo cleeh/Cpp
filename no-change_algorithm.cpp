51e87db317b04a5ffe4d3d75b0abb9bfd83cf164
rockatoo <youlia73@naver.com>
2018-08-19 22:22:41 +0900

    Standard Template Library: Non Change Algorithm - std::remove

--- no-change_algorithm.cpp
+++ no-change_algorithm.cpp
@@ -12,12 +12,22 @@ int main(){
 		std::cout << "Found 30 !" << std::endl;
 	if(iter25 != V.end())
 		std::cout << "Found 25 !" << std::endl;
+	std::cout << std::endl;
 	
 	auto GT25 = [](int n) {return n > 25;};
 	auto itFI = std::find_if(V.begin(), V.end(), GT25);
 	if(itFI != V.end())
 		std::cout << (*itFI) << std::endl;
 	auto ifFI = std::find_if(V.rbegin(), V.rend(), GT25);
+	std::cout << std::endl;
+	
+	std::cout << "V.size(): " << V.size() << std::endl;
+	auto iterR = std::remove(V.begin(), V.end(), 40);
+	if(iterR != V.end()){
+		std::cout << "V.size(): " << V.size() << std::endl;
+		V.erase(iterR, V.end());
+		std::cout << "V.size(): " << V.size() << std::endl;
+	}
 	
 	return 0;
 }
