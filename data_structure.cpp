90328efba35c3dd1403dc0637f803179e63b6063
rockatoo <youlia73@naver.com>
2018-08-19 23:28:55 +0900

    Standard Template Library: Data Structure - Queue

--- data_structure.cpp
+++ data_structure.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <stack>
+#include <queue>
 
 int main(){
 	std::stack<int> S;
@@ -10,6 +11,17 @@ int main(){
 	for(int i = 0; i < count; i++){
 		std::cout << S.top() << std::endl;
 		S.pop();
+	}
+	std::cout << std::endl;
+	
+	std::queue<int> Q;
+	Q.push(1);
+	Q.push(2);
+	Q.push(3);
+	count = Q.size();
+	for(int i = 0; i < count; i++){
+		std::cout << Q.front() << std::endl;
+		Q.pop();
 	}
 	
 	return 0;
